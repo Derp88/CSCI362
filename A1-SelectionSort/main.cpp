7de323f2097f75f8d84ab39146b68204d6c70fa5
Dylan <dgbarkle@iu.edu>
2023-08-30 17:01:47 -0900

    Added makefile and basic output

---
+++ A1-SelectionSort/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+//Function Prototype
+//void SelectionSort()
+
+
+
+int main(){
+    int listOfInts[10];
+    int inputInt;
+    for (int i = 0; i < 10; i++){
+        std::cout << "Number " << i+1 << " out of 10 entered" << std::endl;
+        //std::cin >> inputInt;
+        //std::cout << inputInt;
+    }
+
+
+    return 0;
+}
