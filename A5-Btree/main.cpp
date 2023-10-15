9bfecfb555ff1e75140f6fe0fe7579285457d73f
Dylan <dgbarkle@iu.edu>
2023-10-15 14:40:22 -0900

    Reworked search

--- A5-Btree/main.cpp
+++ A5-Btree/main.cpp
@@ -38,30 +38,20 @@ void insertInt(int newNumber){
 
 Node* findLeafForInt(Node* searchNode, int searchNumber){
     searchNode->sortKeys(); //Make sure our keys are sorted
-    //Find the index of what key we are less than
-    if(!searchNode->isLeaf()){
-        bool foundSubtree = false;
-        //Check and see if our search number is greater than all the keys
-        if (searchNumber > searchNode->keys.back()){
-            foundSubtree = true;
-            findLeafForInt(searchNumber, searchNode->listOfChildren.at(searchNode->keys.size()));
-        }else{
-            for (int i = 0; i < searchNode->keys.size(); i++){ //For every key
-                if(!foundSubtree){ //If we haven't found the correct subtree yet
-                    if(searchNumber < searchNode->keys.at(i)){ //Check and see if our search number is less than the current key
-                        foundSubtree = true;
-                        findLeafForInt(searchNumber, searchNode->listOfChildren.at(i)); //If so, check within the corresponding subtree
-                    }
-                }
-            }
-        }
-    }else{
+    if (searchNode->isLeaf()){
         std::cout << "Node found with keys: " << std::endl;
         for (int i = 0; i < searchNode->keys.size(); i++){
             std::cout << searchNode->keys.at(i) << ", ";
         }
-        std::cout << std::endl;
         return searchNode;
+    }else{
+        int subTreeCounter = 0;
+        for (int i = 0; i < searchNode->keys.size(); i++){
+            if (searchNumber > searchNode->keys.at(subTreeCounter)){
+                subTreeCounter++;
+            }
+        }
+        return findLeafForInt(searchNode->listOfChildren.at(subTreeCounter), searchNumber);
     }
 }
 void printTree(Node* rootNode, std::string path){
@@ -69,23 +59,56 @@ void printTree(Node* rootNode, std::string path){
 }
 
 int main(){
-    std::cout << "Input an integer greater than or equal to 400: ";
+    /*std::cout << "Input an integer greater than or equal to 400: ";
     std::cin >> numOfInts;
     generateRandomInts();
     for (int i = 0; i < listOfRandomInts.size(); i++){
         insertInt(i);
-    }
+    }*/
     rootNode->keys.emplace_back(17);
+
     Node* leftChild = new Node;
     leftChild->keys.emplace_back(3);
     leftChild->keys.emplace_back(8);
+
+    Node* leftGrandchild1 = new Node;
+    leftGrandchild1->keys.emplace_back(1);
+    leftGrandchild1->keys.emplace_back(2);
+    Node* leftGrandchild2 = new Node;
+    leftGrandchild2->keys.emplace_back(6);
+    leftGrandchild2->keys.emplace_back(7);
+    Node* leftGrandchild3 = new Node;
+    leftGrandchild3->keys.emplace_back(12);
+    leftGrandchild3->keys.emplace_back(14);
+    leftGrandchild3->keys.emplace_back(16);
+    leftChild->listOfChildren.emplace_back(leftGrandchild1);
+    leftChild->listOfChildren.emplace_back(leftGrandchild2);
+    leftChild->listOfChildren.emplace_back(leftGrandchild3);
+
     Node* rightChild = new Node;
-    rightChild->keys.emplace_back(24);
+    rightChild->keys.emplace_back(28);
     rightChild->keys.emplace_back(48);
+
+    Node* rightGrandchild1 = new Node;
+    rightGrandchild1->keys.emplace_back(25);
+    rightGrandchild1->keys.emplace_back(26);
+    Node* rightGrandchild2 = new Node;
+    rightGrandchild2->keys.emplace_back(29);
+    rightGrandchild2->keys.emplace_back(45);
+    Node* rightGrandchild3 = new Node;
+    rightGrandchild3->keys.emplace_back(52);
+    rightGrandchild3->keys.emplace_back(53);
+    rightGrandchild3->keys.emplace_back(55);
+    rightGrandchild3->keys.emplace_back(68);
+
+    rightChild->listOfChildren.emplace_back(rightGrandchild1);
+    rightChild->listOfChildren.emplace_back(rightGrandchild2);
+    rightChild->listOfChildren.emplace_back(rightGrandchild3);
+
     rootNode->listOfChildren.emplace_back(leftChild);
     rootNode->listOfChildren.emplace_back(rightChild);
 
-    findLeafForInt(18, rootNode);
+    findLeafForInt(rootNode, 17);
 
     return 0;
 }
