f889ad4aab5c5b1e8a918b8fbda8ab27b9243163
Dylan <dgbarkle@iu.edu>
2023-10-14 17:42:40 -0900

    Added random value generation

---
+++ A5-Btree/README.txt
@@ -0,0 +1,1 @@
+1. Enter an integer greater than or equal to 400.
---
+++ A5-Btree/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+//Vars
+int numOfInts;
+std::vector<int> listOfRandomInts;
+
+//Function prototypes
+void generateRandomInts();
+
+void generateRandomInts(){
+    int upperBound = numOfInts * 3;
+    //Generate a list of all possible numbers
+    std::vector<int> listOfAllPossibleInts;
+    for (int i = 0; i < upperBound+1; i++){
+        listOfAllPossibleInts.emplace_back(i);
+    }
+    //Pop random indexes from the list of all ints
+    srand (time(NULL));
+    for (int i = 0; i < numOfInts; i++){
+        int randomIndex = rand() % listOfAllPossibleInts.size();
+        int randomInt = listOfAllPossibleInts.at(randomIndex);
+        listOfRandomInts.emplace_back(randomInt);
+        //Delete the value we added to our random list from all list
+        listOfAllPossibleInts.erase(listOfAllPossibleInts.begin() + randomIndex); 
+    }
+}
+
+
+
+int main(){
+    std::cout << "Input an integer greater than or equal to 400: ";
+    std::cin >> numOfInts;
+    generateRandomInts();
+    return 0;
+}
